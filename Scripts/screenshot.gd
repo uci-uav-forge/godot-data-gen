0555e102e423925c3b0a2c0795657ed7bb628118
Eric Pedley <ericpedley@gmail.com>
2023-02-28 20:13:01 -0800

    got scene adding and segmentation rendering to work

--- Scripts/screenshot.gd
+++ Scripts/screenshot.gd
@@ -3,38 +3,41 @@ extends Camera
 # it'll take a screenshot once the 
 # scene starts running
 var first = true
-var charactersList
 
-# Called when the node enters the scene tree for the first time.
 func _ready():
-	randomize() # initialize random seed
-	var root = get_tree().get_root().get_node("Root")
-	print(root.get_children())
-	charactersList = root.get_node("Characters").get_children()
+	pass
+	#get_viewport().set_clear_mode(Viewport.CLEAR_MODE_ONLY_NEXT_FRAME)	
 
 func prepForSegmentation(node, color: Color):
 	if node is MeshInstance:
 		# make material 
 		for surface_idx in range(node.mesh.get_surface_count()):
+			node.mesh = node.mesh.duplicate()
 			node.mesh.surface_set_material(surface_idx, SpatialMaterial.new())
 			node.mesh.surface_get_material(surface_idx).flags_unshaded = true
 			node.mesh.surface_get_material(surface_idx).albedo_color = color
 	else:
+		if node == null:
+			return
 		for child in node.get_children():
 			prepForSegmentation(child, color)
 
-		
+func takeScreenshot(file_name):
+	var image = get_viewport().get_texture().get_data()
+	image.save_png("res://%s" % file_name)
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(_delta):
 	if first:
-		print("screenshotting!")
-		var randomCharacter = charactersList[randi() % len(charactersList)]
-		var duplicatedInstance = randomCharacter.duplicate()
-		prepForSegmentation(duplicatedInstance, Color(1, 0, 0, 1))
-		add_child(duplicatedInstance)
-		get_viewport().set_clear_mode(Viewport.CLEAR_MODE_ONLY_NEXT_FRAME)
+		var scene = preload("res://Naruto.tscn")
+		var new_naruto = scene.instance()
+		new_naruto.scale = Vector3(0.05,0.05,0.05)
+		get_tree().get_root().get_node("Root").add_child(new_naruto)
+		takeScreenshot("image.png")
 		yield(VisualServer, "frame_post_draw")
-		first = false
-		var image = get_viewport().get_texture().get_data()
-		image.save_png("res://screenshot.png")
+		prepForSegmentation(new_naruto, Color(1,0,0))
+		
+		#prepForSegmentation(get_node("Floor"), Color(0,0,0))
+		takeScreenshot("mask.png")
+		yield(VisualServer, "frame_post_draw")
+		first=false
