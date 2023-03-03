a6df8b204eab1880eac0fe3b040a5d064125baed
Eric Pedley <ericpedley@gmail.com>
2023-03-03 10:13:00 -0800

    only load crowd scene once

--- Scripts/screenshot.gd
+++ Scripts/screenshot.gd
@@ -1,24 +1,29 @@
 extends Camera
 
-
-# attach this to a Camera node and 
-# it'll take a screenshot once the 
-# scene starts running
 var scene_ready = true
 var index = 0
 var root
+var just_people_nodes = []
+
 func _ready():
 	randomize()
 	root = get_tree().get_root().get_node("Root")
 	var res_directory = Directory.new()
 	res_directory.make_dir("images")
 	res_directory.make_dir("masks")
-	#get_viewport().set_clear_mode(Viewport.CLEAR_MODE_ONLY_NEXT_FRAME)	
+
+	var scene = preload("res://people.tscn")
+	var ppl = scene.instance()
+	var crowd_node = ppl.get_children()[0].get_children()[0].get_children()[0].get_children()[0].get_children()[0].duplicate()
+	var people_regex = RegEx.new()
+	people_regex.compile("AttachHelper")
+	for node in crowd_node.get_children():
+		var result = people_regex.search(node.name) 
+		if result:
+			just_people_nodes.append(node)
 
 func prepForSegmentation(node, color: Color):
 	if node is MeshInstance:
-		# make material 
-
 		node.material_override = SpatialMaterial.new()
 		node.material_override.flags_unshaded=true
 		node.material_override.albedo_color = color
@@ -32,20 +37,7 @@ func takeScreenshot(file_name):
 
 func gen_train_image():
 	scene_ready = false
-	var scene = preload("res://people.tscn")
-	var ppl = scene.instance()
-	var crowd_node = ppl.get_children()[0].get_children()[0].get_children()[0].get_children()[0].get_children()[0].duplicate()
-	crowd_node.scale=Vector3(0.1,0.1,0.1)		
-	var people_regex = RegEx.new()
-	people_regex.compile("AttachHelper")
-	var just_people_nodes = []
-	for node in crowd_node.get_children():
-		var result = people_regex.search(node.name) 
-		if result:
-			just_people_nodes.append(node)
-	#print(just_people_nodes)
 	var random_person = just_people_nodes[randi()%len(just_people_nodes)].duplicate()
-	#root.add_child(crowd_node)
 	root.add_child(random_person)
 	random_person.translation=Vector3(randi()%10,randi()%10,randi()%10)
 	random_person.scale=Vector3.ONE*0.05
