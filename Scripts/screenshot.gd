933894767203b7e02fc18f6123eaa9d78aacd14e
Eric Pedley <ericpedley@gmail.com>
2023-03-11 18:25:04 -0800

    fix shape drawing still has bug where sometimes shapes aren't drawn to image nor mask (might be because they're outside of the viewport?)

--- Scripts/screenshot.gd
+++ Scripts/screenshot.gd
@@ -6,10 +6,12 @@ onready var root = get_tree().get_root().get_node("Root")
 onready var just_people_nodes = []
 onready var shapes_list = preload("res://Shapes.tscn").instance().get_children()
 onready var symbols = "ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789"
+onready var res_directory = Directory.new()
 
 func _ready():
 	randomize()
-	var res_directory = Directory.new()
+	
+	res_directory.open("/tmp")
 	res_directory.make_dir("images")
 	res_directory.make_dir("masks")
 
@@ -28,56 +30,70 @@ func prepForSegmentation(node, color: Color):
 		node.material_override = SpatialMaterial.new()
 		node.material_override.flags_unshaded=true
 		node.material_override.albedo_color = color
-	else:
-		for child in node.get_children():
-			prepForSegmentation(child, color)
+	elif node is Label3D:
+		node.modulate = color
+		node.shaded=false
+	for child in node.get_children():
+		prepForSegmentation(child, color)
 
 func takeScreenshot(file_name):
 	var image = get_viewport().get_texture().get_data()
-	image.save_png("res://%s" % file_name)
+	image.save_png("/tmp/%s" % file_name)
 	
 func makeShapeTarget():
 	var shape = shapes_list[randi()%len(shapes_list)].duplicate()
 	shape.material_override = SpatialMaterial.new()
-	#shape.material_override.flags_unshaded=true
 	shape.material_override.albedo_color = Color(randf(), randf(), randf())
 	var label = Label3D.new()
 	label.text = symbols[randi()%len(symbols)]
 	label.font = DynamicFont.new()
-	label.font.font_data = load("res://fonts/OpenSans/OpenSans-Bold.ttf")#DynamicFontData.new()
-	#label.font.font_data.font_path = 
+	label.font.font_data = load("res://fonts/OpenSans/OpenSans-Bold.ttf")
 	label.font.size = 100
 	label.modulate = Color(randf(), randf(), randf())
 	label.rotate_x(-PI/2)
 	shape.add_child(label)
 	label.translate(Vector3(0,0.01,0))
-	return shape
+	return [shape, shape.name]
 	
 
 func gen_train_image():
+	print("gen image")
 	scene_ready = false
-	var random_person = just_people_nodes[randi()%len(just_people_nodes)].duplicate()
-	root.add_child(random_person)
+	var target_objects = []
+	var target_labels = []
+	if (randi()%2)>0:
+		var random_person = just_people_nodes[randi()%len(just_people_nodes)].duplicate()
+		root.add_child(random_person)
+		random_person.translation= 10 * Vector3(randf()-0.5,randf()-0.5,randf()-0.5)
+		random_person.rotate_y(randf()*TAU)
+		random_person.scale=Vector3.ONE*0.05
+		target_objects.append(random_person)
+		target_labels.append("person")
 	var num_shapes = randi()%10
-	var shapes = []
 	for _s in range(num_shapes):
-		var shape = makeShapeTarget()
+		var shape_and_name = makeShapeTarget()
+		var shape = shape_and_name[0]
+		var shape_name = shape_and_name[1]
 		root.add_child(shape)
-		shape.translation = Vector3(randi()%10,randi()%10,randi()%10)
-		shapes.append(shape)
-	random_person.translation=Vector3(randi()%10,randi()%10,randi()%10)
-	random_person.scale=Vector3.ONE*0.05
+		shape.translation = 10 * Vector3(randf()-0.5,randf()-0.5,randf()-0.5)
+		shape.rotate_y(randf()*TAU)
+		target_objects.append(shape)
+		target_labels.append(shape_name)
+		print(shape_name)
+	
 	yield(VisualServer, "frame_post_draw")
 	takeScreenshot("images/image%s.png" % index)
-	prepForSegmentation(random_person, Color(1,1,1))
 	prepForSegmentation(root.get_node("Floor"), Color(0,0,0))
-	for shape in shapes:
-		prepForSegmentation(shape, Color(1,0,0))
-	yield(VisualServer, "frame_post_draw")
-	takeScreenshot("masks/mask%s.png" % index)
-	random_person.free()
-	for shape in shapes:
-		shape.free()
+	
+	for obj in target_objects:
+		obj.hide()
+	for i in target_objects.size():
+		target_objects[i].show()
+		prepForSegmentation(target_objects[i], Color(1,1,1))
+		yield(VisualServer, "frame_post_draw")
+		takeScreenshot("masks/mask%s%s%s.png" % [index, target_labels[i], i])
+		yield(VisualServer, "frame_post_draw")
+		target_objects[i].free()
 	root.get_node("Floor").material_override = null
 	index += 1
 	scene_ready = true
