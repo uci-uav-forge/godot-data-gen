dfef95f9edf2177d70a40d54f024433e19e13161
Eric Pedley <ericpedley@gmail.com>
2023-04-18 10:46:51 -0700

    make scales more like real life

--- Scripts/screenshot.gd
+++ Scripts/screenshot.gd
@@ -80,45 +80,53 @@ func makeShapeTarget():
 	label.modulate = Color(randf(), randf(), randf())
 	label.rotate_x(-PI/2)
 	shape.add_child(label)
-	label.translate(Vector3(0,0.01,0))
+	label.translate(0.1*Vector3.BACK)
 	return [shape, shape.name]
 
 func get_target_objects_and_labels():
 	var target_objects = []
 	var target_labels = []
-	if (randi()%2)>0:
+	if randi()%3==0:
 		var random_person = just_people_nodes[randi()%len(just_people_nodes)].duplicate()
 		root.add_child(random_person)
-		random_person.translation= Vector3.ZERO
-		random_person.scale=Vector3.ONE*0.5
+		random_person.translation = 4*Vector3.UP
+		random_person.scale= 0.1*Vector3.ONE
 		target_objects.append(random_person)
 		target_labels.append("person")
-	var num_shapes = randi()%10
+	var num_shapes = randi()%4
 	for _s in range(num_shapes):
 		var shape_and_name = makeShapeTarget()
 		var shape = shape_and_name[0]
 		var shape_name = shape_and_name[1]
 		root.add_child(shape)
-		shape.translation = 0.01*Vector3.UP
-		shape.scale=Vector3.ONE*1.5
+		shape.translation = 0.1*Vector3.UP
+		shape.scale=Vector3.ONE*0.8
 		target_objects.append(shape)
 		target_labels.append(shape_name)
 	return [target_objects, target_labels]
 
 func gen_train_image():
-	scene_ready = false	
+	scene_ready = false
 	res_directory.make_dir("masks/%s" % index)
 	var target_objects_and_labels = get_target_objects_and_labels()
 	var target_objects = target_objects_and_labels[0]
 	var target_labels = target_objects_and_labels[1]
+	self.rotation_degrees = Vector3(rand_range(-60, -120), rand_range(0,360), 0)
+	var dist_from_center = self.translation.y * tan(-PI/2-self.rotation.x)
+	var picture_center = Vector3(0,0,dist_from_center).rotated(Vector3.UP, self.rotation.y)
+	
+	var positions = []
+	for x in range(-5, 6, 2):
+		for z in range(-5, 6, 2):
+			positions.append(Vector3(x,0,z))
+	positions.shuffle()
 	
 	for obj in target_objects:
 		obj.rotate_y(randf()*TAU)
-		obj.translate(50 * Vector3(randf()-0.5,0,randf()-0.5))
+		obj.translation+=picture_center + positions.pop_front() + Vector3(randf()-0.5, 0, randf()-0.5)
 		obj.scale_object_local(rand_range(0.9, 1.1)*Vector3(rand_range(0.8, 1.2),rand_range(0.8, 1.2),rand_range(0.8, 1.2)))
 
-	global_light.light_energy = randf()*1.2
-	self.rotation_degrees = Vector3(rand_range(-80, -100), rand_range(0,360), rand_range(0, 360))
+	global_light.light_energy = randf()*2
 	var background_path = backgrounds_directory_list[randi()%len(backgrounds_directory_list)]
 	var background = load("res://backgrounds/%s" % background_path)
 	world_floor.material_override = SpatialMaterial.new()
@@ -127,7 +135,6 @@ func gen_train_image():
 	
 	yield(VisualServer, "frame_post_draw")
 	takeScreenshot("images/image%s.png" % index)
-
 	prepForSegmentation(world_floor, Color.black)
 	for obj in target_objects:
 		obj.hide()
