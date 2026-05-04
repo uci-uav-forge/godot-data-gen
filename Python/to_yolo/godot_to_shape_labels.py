c271e7966c810ed6d1df5996e3961f9f8842be17
Arnav Doshi <asdoshi1@uci.edu>
2026-05-04 15:13:08 -0700

    Confingure godot_to_shape_labels for my windows setup. Only include mannequin and tent labels.

--- Python/to_yolo/godot_to_shape_labels.py
+++ Python/to_yolo/godot_to_shape_labels.py
@@ -8,13 +8,30 @@ from tqdm import tqdm
 from pathlib import Path
 import yaml
 
-user = os.environ["USER"]
-# for linux
-dataset_dirs = sorted(list(Path(f"/home/{user}/.local/share/godot/app_userdata/forge-godot").glob('godot_data*')))
+# Determine OS and set data directory
+import platform
+import sys
+
+if platform.system() == "Windows":
+    # For Windows, use APPDATA environment variable
+    appdata = os.environ.get("APPDATA")
+    data_base = Path(appdata) / "Godot" / "app_userdata" / "forge-godot"
+else:
+    # For Linux
+    user = os.environ["USER"]
+    data_base = Path(f"/home/{user}/.local/share/godot/app_userdata/forge-godot")
+
+# Find all godot_data* folders
+dataset_dirs = sorted(list(data_base.glob('godot_data*')))
+
+if not dataset_dirs:
+    print(f"No godot_data* folders found in {data_base}")
+    print("Have you run the Godot project to generate data?")
+    sys.exit(1)
 
 for idx, dataset_dir in enumerate(dataset_dirs):
     num_frames = sum(1 for _ in (dataset_dir / 'images').glob("*.png"))
-    print(f"{idx}\t{num_frames} frames\t{str(dataset_dir).split('logs/')[-1]}")
+    print(f"{idx}\t{num_frames} frames\t{dataset_dir.name}")
 
 dir_selected = input(
     "Enter index of log directory to process (default -1, last one in list): "
@@ -24,11 +41,6 @@ if dir_selected == "":
 else:
     dir_index = int(dir_selected)
 INPUT_DIR = dataset_dirs[dir_index]
-
-# for windows (change username)
-# input_dir = "/mnt/c/Users/sch90/AppData/Roaming/Godot/app_userdata/forge-godot/godot_data"
-
-# INPUT_DIR = "C:\\Users\\Bran\\AppData\\Roaming\\Godot\\app_userdata\\forge-godot\\godot_data_1730248253"
 
 
 def gen_img(num, num_images, input_dir, output_dir, shapes_to_categories):
